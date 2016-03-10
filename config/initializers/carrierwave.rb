eb3e8ceacb63913327c06014a246984a6082cc02
yukihonda <honda@strawcorp.com>
2016-03-10 19:28:53 +0900

    リージョンを東京に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,15 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = 'yukihonda'
-        config.asset_host = 'https://s3.amazonaws.com/yukihonda'
+        config.fog_directory  = 'straw-image'
+        config.asset_host = 'https://s3.amazonaws.com/straw-image'
     when 'production'
-        config.fog_directory  = 'yukihonda'
-        config.asset_host = 'https://s3.amazonaws.com/yukihonda'
+        config.fog_directory  = 'straw-image'
+        config.asset_host = 'https://s3.amazonaws.com/straw-image'
     end
 end
