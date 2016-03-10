4d625d61dbb28d67f39b3addbdd362c7d6bf3d9b
yukihonda <honda@strawcorp.com>
2016-03-10 23:00:27 +0900

    gem configの導入

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,17 +2,18 @@ CarrierWave.configure do |config|
     config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
+    aws_access_key_id:       Settings.aws_key,
+    aws_secret_access_key:   Settings.secret_access_key,
+    region:                  Settings.region,
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = 'straw-image'
-        config.asset_host = 'https://s3.amazonaws.com/straw-image'
+      config.fog_directory = Settings.backet
+      config.asset_host = Settings.asset_host
     when 'production'
-        config.fog_directory  = 'straw-image'
-        config.asset_host = 'https://s3.amazonaws.com/straw-image'
+      config.fog_directory = Settings.backet
+      config.asset_host = Settings.asset_host
     end
+
 end
